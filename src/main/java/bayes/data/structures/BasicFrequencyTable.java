c5c4c65b7a84018d56d303655676769379224879
Rashmi <rashmiprabhath@gmail.com>
2017-09-29 12:14:16 +0530

    Create ArrayFrequecyTable class. define all variables, generating maps are done.

--- src/main/java/bayes/data/structures/BasicFrequencyTable.java
+++ src/main/java/bayes/data/structures/BasicFrequencyTable.java
@@ -1,8 +1,43 @@
 package bayes.data.structures;
 
+import java.util.Map;
+
 /**
  * Created by Rashmi on 9/28/2017.
  */
-public interface BasicFrequencyTable
+public abstract class BasicFrequencyTable
 {
+	String className;
+	String attributeName;
+	int classCardinality;
+	int attributeCardinality;
+	Map<String, Integer> classMap;
+
+	public BasicFrequencyTable( String className, String attributeName, String[] classValues, int attributeCardinality )
+	{
+		this.className = className;
+		this.attributeName = attributeName;
+		generateClassMap( classValues );
+		this.classCardinality = classValues.length;
+		this.attributeCardinality = attributeCardinality;
+	}
+
+	public void generateClassMap( String[] classValues )
+	{
+		int len = classValues.length;
+		for ( int i = 0; i < len; i++ )
+		{
+			classMap.put( classValues[i], i );
+		}
+	}
+
+	public int getClassIndex( String value )
+	{
+		return ( int ) classMap.get( value );
+	}
+
+	abstract int getAttributeIndex( String value );
+
+	abstract double getProbabilty( String attributeValue, String classValue );
+
 }
